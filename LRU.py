6d29d6ac1259f4d68b2864bd11d2df7f26999b35
praveenreddypingili1381996 <praveenreddypingili123@msitprogram.net>
2020-04-14 13:21:56 +0530

    added methods

--- LRU.py
+++ LRU.py
@@ -2,5 +2,12 @@ class LRU:
 
     def _init_(self):
         self.list = []
-        
+
+    def put(self):
+        pass   
+    def get(self):
+        pass
+    def get_cache(self):
+        pass               
+
 
