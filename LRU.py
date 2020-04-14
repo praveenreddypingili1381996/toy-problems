0b0d04807d557402df029da8096f6725398f517c
praveenreddypingili1381996 <praveenreddypingili123@msitprogram.net>
2020-04-14 13:49:15 +0530

    LRU test class is created and functions are also written

--- LRU.py
+++ LRU.py
@@ -7,7 +7,56 @@ class LRU:
         pass   
     def get(self):
         pass
-    def get_cache(self):
-        pass               
+    def getcache(self):
+        pass    
+
+
+class LRU_test:
+
+    def _init_(self):
+        self.toy = LRU()
+        self.total = 0
+
+    def test_put(self):
+        self.toy.put(3)
+        try:
+            assert self.toy.list == [3]
+            self.total = self.total+1
+        except Exception as e:
+            print("failput")    
+
+    def test_get(self):
+        value = self.toy.get()
+
+         try:
+            assert self.toy.list == []
+            self.total = self.total+1
+        except Exception as e:
+            print("failget")    
+        
+    def test_getcache(self):
+        newvalue = self.toy.getcache()
+
+        try:
+            assert self.toy.list == newvalue
+            self.total = self.total+1
+        except Exception as e:
+            print("failgetcache")  
+
+
+def main():
+    box = LRU_test()
+    box.test_put()
+    box.test_get()
+    box.test_getcache()
+    if box.total == 3:
+        print("All test cases are passed")
+if__name__ == "__main__":
+    main()
+               
+
+
+
+
 
 
